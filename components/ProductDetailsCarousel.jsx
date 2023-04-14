124751db80d1ebfac41b71fe0b64598cd0d198bd
ruggylandweb <ruggyland.web@gmail.com>
2023-04-15 00:23:11 +0530

    initial commit

---
+++ components/ProductDetailsCarousel.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+
+const ProductDetailsCarousel = ({ images }) => {
+  return (
+    <div className="text-white text-[20px] w-full max-w-[1360px] mx-auto sticky top-[50px]">
+      <Carousel
+        infiniteLoop={true}
+        showIndicators={false}
+        showStatus={false}
+        thumbWidth={60}
+        className="productCarousel"
+      >
+        {images.map((img) => (
+          <img
+            key={img.id}
+            src={img.attributes.url}
+            alt={img.attributes.name}
+          />
+        ))}
+        {/* <img src="/p1.webp" />
+        <img src="/p2.webp" />
+        <img src="/p3.webp" />
+        <img src="/p4.webp" /> */}
+      </Carousel>
+    </div>
+  );
+};
+
+export default ProductDetailsCarousel;
